2352f250e33f3fe88aec9b0be9bf4c8b2a0ace70
Arnabmukherjee2122 <aec.cse.arnabmukherjee2001@gmail.com>
2024-06-19 22:06:03 +0530

    second commit

--- model/db.js
+++ model/db.js
@@ -1,8 +1,15 @@
-const mongoose = require('mongoose')
-mongoose.connect("mongodb://localhost:27017/Fit-O-Meter",{ useNewUrlParser:true })
-.then(()=>{
+const mongoose = require('mongoose');
+
+// URL-encoded password if it contains special characters
+const password = encodeURIComponent('arnab@2001');
+
+// Construct the connection string with the URL-encoded password
+const uri = `mongodb+srv://aeccsearnabmukherjee2001:${password}@cluster0.kawowny.mongodb.net/fit-O-meter?retryWrites=true&w=majority`;
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => {
     console.log("Database connected...");
 })
-.catch((err)=>{
-    console.log(err);
-})
+.catch((err) => {
+    console.error("Connection error", err);
+});
